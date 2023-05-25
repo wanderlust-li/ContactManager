b47340b775f0903c0fac127027eb9ea785af26b6
wanderlust-li <zhekalichm4n@icloud.com>
2023-05-25 20:54:01 +0300

    db context

---
+++ ContactManager.Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using ContactManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactManager.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext()
+    {
+    }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> context) : base(context)
+    {
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=ContentManager;Trusted_Connection=True;");
+    }
+    
+    public DbSet<Contact> Contacts { get; set; }
+}
