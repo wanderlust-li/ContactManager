88c1a22f9edd8a0ef9ac3baee2067f1e2699123d
wanderlust-li <zhekalichm4n@icloud.com>
2023-05-25 21:11:43 +0300

    add migration

--- ContactManager.Data/ApplicationDbContext.cs
+++ ContactManager.Data/ApplicationDbContext.cs
@@ -12,11 +12,23 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> context) : base(context)
     {
     }
-    
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=ContentManager;Trusted_Connection=True;");
     }
-    
+
     public DbSet<Contact> Contacts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Contact>().HasData(
+            new Contact
+            {
+                Id = 1, Name = "Yevhenii", Surname = "Lichman", Email = "yevhenii@gmail.com",
+                PhoneNumber = "9999999"
+            });
+    }
 }
---
+++ ContactManager.Data/Migrations/20230525180925_addContactToDb.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactManager.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addContactToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "Id", "Email", "Name", "PhoneNumber", "Surname" },
+                values: new object[] { 1, "yevhenii@gmail.com", "Yevhenii", "9999999", "Lichman" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
