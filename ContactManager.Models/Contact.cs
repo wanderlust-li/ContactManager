76cf6bdf5cb645e75419d3db77475da0de03ed50
wanderlust-li <zhekalichm4n@icloud.com>
2023-05-25 20:49:35 +0300

    contact model

---
+++ ContactManager.Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManager.Models;
+
+public class Contact
+{
+    [Key]
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public string Surname { get; set; }
+    
+    public string Email { get; set; }
+    
+    public string PhoneNumber { get; set; }
+}
