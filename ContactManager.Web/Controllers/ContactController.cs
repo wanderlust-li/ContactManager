15b4112a65dc59d0c4c77537875c4e591a91ef47
wanderlust-li <zhekalichm4n@icloud.com>
2023-05-25 21:31:16 +0300

    get method in controller

--- ContactManager.Web/Controllers/ContactController.cs
+++ ContactManager.Web/Controllers/ContactController.cs
@@ -1,12 +1,22 @@
+using ContactManager.Data;
+using ContactManager.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ContactManager.Web.Controllers;
 
 public class ContactController : Controller
 {
+    private readonly ApplicationDbContext _context;
+
+    public ContactController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
     // GET
     public IActionResult Index()
     {
-        return View();
+        IEnumerable<Contact> allContacts = _context.Contacts;
+
+        return View(allContacts);
     }
 }
