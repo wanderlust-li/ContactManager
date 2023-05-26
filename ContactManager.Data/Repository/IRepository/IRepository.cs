1f5aafc53b76691578552fff9ccbca310dd191fc
wanderlust-li <zhekalichm4n@icloud.com>
2023-05-26 12:35:52 +0300

    create repository

---
+++ ContactManager.Data/Repository/IRepository/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+
+namespace ContactManager.Data.Repository.IRepository;
+
+public interface IRepository<T> where T : class
+{
+    IEnumerable<T> GetAll();
+    T Get(Expression<Func<T, bool>> filter);
+    void Add(T entity);
+    void Remove(T entity);
+}
---
+++ ContactManager.Data/Repository/Repository.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq.Expressions;
+using ContactManager.Data.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactManager.Data.Repository;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly ApplicationDbContext _db;
+    internal DbSet<T> dbSet;
+
+    public Repository(ApplicationDbContext db)
+    {
+        _db = db;
+        this.dbSet = _db.Set<T>();
+    }
+
+    public IEnumerable<T> GetAll()
+    {
+        IQueryable<T> query = dbSet;
+        return query.ToList();
+    }
+
+    public T Get(Expression<Func<T, bool>> filter)
+    {
+        IQueryable<T> query = dbSet;
+        query = query.Where(filter);
+        return query.FirstOrDefault();
+    }
+
+    public void Add(T entity)
+    {
+        dbSet.Add(entity);
+    }
+
+    public void Remove(T entity)
+    {
+        dbSet.Remove(entity);
+    }
+}
