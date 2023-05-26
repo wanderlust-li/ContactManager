7f7f4c2b9fb525fb26ef715c39991f57b787082a
wanderlust-li <zhekalichm4n@icloud.com>
2023-05-26 12:41:02 +0300

    contact repository

---
+++ ContactManager.Data/Repository/ContactRepository.cs
@@ -0,0 +1,18 @@
+﻿using ContactManager.Data.Repository.IRepository;
+using ContactManager.Models;
+
+namespace ContactManager.Data.Repository;
+
+public class ContactRepository : Repository<Contact>, IContactRepository
+{
+    private ApplicationDbContext _db;
+    public ContactRepository(ApplicationDbContext db) : base(db)
+    {
+        _db = db;
+    }
+
+    public void Update(Contact obj)
+    {
+        _db.Contacts.Update(obj);
+    }
+}
---
+++ ContactManager.Data/Repository/IRepository/IContactRepository.cs
@@ -0,0 +1,8 @@
+﻿using ContactManager.Models;
+
+namespace ContactManager.Data.Repository.IRepository;
+
+public interface IContactRepository :IRepository<Contact>
+{
+    void Update(Contact obj);
+}
